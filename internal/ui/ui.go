22553285b8f60bfc0f96a5db884624b8966dcbdf
Bostigger <tricksteck@gmail.com>
2024-01-23 02:25:08 +0000

    Implement dynamic Bot typing message

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -8,12 +8,14 @@ import (
 // CreateMainLayout sets up the UI and returns the top-level layout.
 func CreateMainLayout(app *tview.Application) *tview.Flex {
 	titleBox := widgets.CreateTitleBox()
+	subtitleBox := widgets.CreateSubtitle()
 	chatArea := widgets.CreateChatArea()
 	inputField := widgets.SetupInputField(app, chatArea)
 
 	layout := tview.NewFlex().
 		SetDirection(tview.FlexRow).
 		AddItem(titleBox, 0, 5, false).
+		AddItem(subtitleBox, 0, 1, false).
 		AddItem(chatArea, 0, 10, false).
 		AddItem(inputField, 3, 1, true)
 	return layout
